22d0e672ff93ec054bcd5b4e6760287cf1437113
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2024-01-10 17:00:21 -0300

    chore: configs typeorm and env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })

7ec1f7a5912a7273fec250a1a98964266d5822e7
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2024-01-10 18:02:27 -0300

    feat: add users controllers

---
+++ src/app/users/users.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('api/v1/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  public async store(@Body() body: CreateUserDto) {
+    return await this.usersService.store(body);
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  public async show(@Param('id', new ParseUUIDPipe()) id: string) {
+    return await this.usersService.findOneOrFail({ id });
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  public async index() {
+    return await this.usersService.findAll();
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  public async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateUserDto,
+  ) {
+    return await this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.usersService.destroy(id);
+  }
+}

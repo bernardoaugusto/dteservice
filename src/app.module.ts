2bae1c2d61c5bb8528e1933db1c83951c8e1a54d
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2024-01-10 17:03:59 -0300

    feat: remove Hello World

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
